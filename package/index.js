83b32f5ecfb46308765894228547e27edb29b457
colohr <colohr.code@gmail.com>
2018-06-23 07:17:47 -0500

    hyper

--- package/index.js
+++ package/index.js
@@ -1,5 +1,11 @@
 //exports
 module.exports = {
+	get data(){
+		return {
+			get base(){ return require('data.base') },
+			get stream(){ return require('data.stream') }
+		}
+	},
 	get environment(){ return require('./environment') },
 	get hyper(){ return require('hyper') }
 }
